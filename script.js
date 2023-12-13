3d7b0f27cc3fd699d79b4e6d796aaa0bf41bd95a
camara <ninobrowns028@gmail.com>
2023-12-13 13:12:56 +0100

    modification dossier

--- script.js
+++ script.js
@@ -17,45 +17,41 @@ window.onscroll = () => {
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height) {
+        if (top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id +']').classList.add('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
         };
     });
-     /*====================== sticky navbar ====================*/
-     let header = document.querySelector('header');
+    /*====================== sticky navbar ====================*/
+    let header = document.querySelector('header');
 
-     header.classList.toggle('sticky', window.scrollY > 100);
+    header.classList.toggle('sticky', window.scrollY > 100);
 
-      /*====================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
+    /*====================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
     menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active'); 
+    navbar.classList.remove('active');
 };
 
 
 
 // ========================= scroll reveal ==========================
-ScrollReveal({ 
-    // reset: true,
+ScrollReveal({
+    //  reset: true,
     distance: '80px',
     duration: 2000,
     delay: 200
 });
-   
+
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .services-container, .works-box, .contact form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'rigth' });
-   
-
-
-
 
 
 /*====================== typed js====================*/
-const typed = new typed('multiple-text', {
+const typed = new Typed('.multiple-text', {
     strings: ['Developer', 'Full-Stack Developer'],
     typeSpeed: 100,
     backSpeed: 100,
